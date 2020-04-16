dd2328bb24b465145f3005d3e1aa928de0903fe9
Evandro Chagas <ev.crr97@gmail.com>
2020-04-16 18:42:46 -0300

    iterator

--- include/ket_bits/quant.hpp
+++ include/ket_bits/quant.hpp
@@ -2,15 +2,44 @@
 #include "base.hpp"
 
 namespace ket {
+
+    class iterator;
+
     class quant {
     public:
         quant(size_t size, bool dirty = false);
+
+        quant operator()(size_t idx) const;
 
         const std::vector<std::shared_ptr<base::qubit>>& get_base_qubits() const;
 
         size_t len() const;
 
+        iterator begin();
+        iterator end();
+
     private:
+        quant(const std::vector<std::shared_ptr<base::qubit>> &qubits);
+
         std::vector<std::shared_ptr<base::qubit>> qubits;
+
+        friend class iterator;
+    };
+
+    class iterator {
+    public:
+        quant operator*();
+
+        const iterator& operator++();
+
+        bool operator!=(const iterator& other) const;
+        
+    private:
+        iterator(const std::vector<std::shared_ptr<base::qubit>> &qubits);
+        iterator();
+
+        const std::vector<std::shared_ptr<base::qubit>> *qubits;
+        size_t index;
+        friend class quant;
     };
 }
--- src/quant.cpp
+++ src/quant.cpp
@@ -8,10 +8,49 @@ quant::quant(size_t size, bool dirty) {
         qubits.push_back(ket_hdl.alloc(dirty));
 }
 
+quant::quant(const std::vector<std::shared_ptr<base::qubit>>& qubits) :
+    qubits{qubits}
+    {}
+
+quant quant::operator()(size_t idx) const {
+    return quant{{qubits[idx]}};
+}
+
 const std::vector<std::shared_ptr<base::qubit>>& quant::get_base_qubits() const {
     return qubits;
 }
 
 size_t quant::len() const {
     return qubits.size();
+}
+
+iterator quant::begin() {
+    return iterator{qubits};
+}
+
+iterator quant::end() {
+    return iterator{};
+}
+
+iterator::iterator(const std::vector<std::shared_ptr<base::qubit>> &qubits) :
+    qubits{&qubits},
+    index{0}
+    {}
+    
+iterator::iterator() : 
+    qubits{nullptr},
+    index{SIZE_MAX}
+    {}
+
+quant iterator::operator*() {
+    return quant{{qubits[index]}};
+}
+
+const iterator& iterator::operator++() {
+    index++;
+    return *this;
+}
+
+bool iterator::operator!=(const iterator& other) const {
+    return index != other.index or qubits != other.qubits;
 }
