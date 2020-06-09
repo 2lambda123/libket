f438a05c144f8f17cb7e67fa7cef7c9e94d04295
Evandro Chagas <ev.crr97@gmail.com>
2020-06-09 14:52:24 -0300

    License

--- src/wrapper.cpp
+++ src/wrapper.cpp
@@ -1,3 +1,27 @@
+/* MIT License
+ * 
+ * Copyright (c) 2020 Evandro Chagas Ribeiro da Rosa <evandro.crr@posgrad.ufsc.br>
+ * Copyright (c) 2020 Rafael de Santiago <r.santiago@ufsc.br>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 #include "../include/ket"
 #include "../include/ket.h"
 #include <ctime>
@@ -140,4 +164,3 @@ void ket_dump(ket_quant* q) {
 void ket_if_then(ket_future* c, void (*t)(void), void (*e)(void), ket_process* p) {
     if_then(*static_cast<future*>(c), t, e, *static_cast<process*>(p));
 }
-
