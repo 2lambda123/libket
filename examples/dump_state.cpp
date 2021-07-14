e374f6dc7cdc86365629cc0fc2f8ac206cd7e982
Evandro Chagas <ev.crr97@gmail.com>
2021-07-14 19:57:11 -0300

    remove __gnu_cxx

--- examples/dump_state.cpp
+++ examples/dump_state.cpp
@@ -4,29 +4,10 @@ using namespace ket;
 
 
 int main() {
-    
-    auto state0  = run([]{
-        quant q{3};
-        H(q);
-        S(q(0));
-        T(q(1));
-        dump state{q};
-        state.get();
-        return state;
-    });
-
-    auto state1  = run([]{
-        quant q{3};
-        H(q);
-        X(q);
-        T(q(1));
-        S(q(0));
-        dump state{q};
-        state.get();
-        return state;
-    });
-
-    std::cout << (state0 == state1? "Equal" : "Diferrent") << " states" << std::endl;;
+  
+    quant q{1};
+    RX(-M_PI_2, q);
+    measure(q).get();
     
     return 0;   
 }
